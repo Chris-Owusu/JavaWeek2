0a445633f6fdfe168ff5434fa65acb3893b2f066
Chris-Owusu <christopher.owusu@amalitech.com>
2024-05-12 14:11:50 +0000

    fix question 3

--- src/Question3/Vehicle.java
+++ src/Question3/Vehicle.java
@@ -7,7 +7,7 @@ abstract class Vehicle {
 
     abstract void accelerate();
 
-    void printDetails(String make, String model, String year) {
+    void printDetails(String make, String model, int year) {
         System.out.println("Make: " + make);
         System.out.println("Model: " + model);
         System.out.println("Year: " + year);
@@ -18,7 +18,13 @@ abstract class Vehicle {
 class Main {
     public static void main(String[] args) {
         Vehicle car = new Car("Honda", "Accord", 2022);
+        car.accelerate();
+        car.printDetails("Honda", "Accord", 2022);
+
+        System.out.println( );
 
         Vehicle bike = new Bike("BMW", "P40", 2012);
+        bike.accelerate();
+        bike.printDetails("BMW", "P40", 2012);
     }
 }
